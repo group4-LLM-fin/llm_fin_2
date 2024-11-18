62615d729356eb26652cda91954779c4f4fd40c5
mpquochung <maphquochung@gmail.com>
2024-11-18 14:23:09 +0700

    check ocr

--- Home.py
+++ Home.py
@@ -4,3 +4,13 @@ st.set_page_config(
     page_icon="graphics/anya_logo.png" 
 )
 st.title('Welcome')
+
+import os
+
+# Check if the directory exists
+print("Checking tessdata directory contents:")
+os.system("ls -la /usr/share/tesseract-ocr/4.00/tessdata")
+
+# Check if the file exists in the working directory
+print("Checking local tessdata directory contents:")
+os.system("ls -la ./tessdata")
